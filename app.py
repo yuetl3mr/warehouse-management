5a442e09ce7a244ce8d94502faef92e9347c4787
Nguyen <huynhkhoinguyen2004@gmail.com>
2024-11-07 19:47:07 +0700

    signup

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
