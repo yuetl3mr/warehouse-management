47e4026ddf482ed011c17d96b8782227f9525b6c
Nguyen <huynhkhoinguyen2004@gmail.com>
2024-11-07 15:20:12 +0700

    base

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from controller import home_bp  
+from flask import render_template
 
 app = Flask(__name__)
-app.register_blueprint(home_bp)  
+app.register_blueprint(home_bp)
+
+@app.route('/')
+def index():
+    return render_template('index.html', total_products=100, total_orders=50, low_stock=10)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
